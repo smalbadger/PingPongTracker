c8a48d4b99b420728f0f8deb1f6a4131f5fdb631
Sam Badger <smalbadger@email.arizonae.edu>
2018-11-06 01:01:10 +0800

    Can add 3D points

--- viz/ppViz.py
+++ viz/ppViz.py
@@ -29,20 +29,34 @@ from PySide2.QtCore import Qt
 
 import imageio
 import numpy as np
+import csv
 
 #############################################################################
 #                            Globals Begin                                  #
 #############################################################################
-SHIFT = False
-CTRL  = False
+MOUSE_BUTTON = None
+MOVING = None
+START_X = None
+START_Y = None
+ANGLE = 0
+ANGLE2 = 0
+
+
 CUR_SEQUENCE = 0
 CUR_FRAME = 0
 FRAME_RATE = .25
 NUMBER_OF_FRAMES = 1
 PLAYING = False
 VIDEO_DIR = "../TestVideos/"
+
 BALL_2D_COORDS_DIR = ""
-BALL_3D_COORDS_DIR = ""
+CUR_2D_FILE = None
+READER_2D = None
+
+
+BALL_3D_COORDS_DIR = "../Test3DCoords/"
+CUR_3D_FILE = None
+READER_3D = None
 
 
 R1 = np.array([[ .96428, -.26485, -.0024166],
@@ -72,23 +86,33 @@ t3 = np.array([[-.060452],
 c1 = np.matmul(-1 * np.linalg.inv(R1), t1)
 c2 = np.matmul(-1 * np.linalg.inv(R2), t2)
 c3 = np.matmul(-1 * np.linalg.inv(R3), t3)
-'''
-print(R1)
-print(R1[0])
-print(R1[0][0])
-print(R1[0,:])
-print(R1[0,0])
-'''              
+
+ 
 def updateSequence(seqNum):
+    global BALL_3D_COORDS_DIR
+    global VIDEO_DIR
     global CUR_SEQUENCE 
     global CUR_FRAME
     global NUMBER_OF_FRAMES
+    global CUR_3D_FILE
+    global READER_3D
+    
     CUR_SEQUENCE = seqNum
-    videoFiles = sorted([name for name in os.listdir(VIDEO_DIR)])[:10]
-    vid = imageio.get_reader(VIDEO_DIR + videoFiles[CUR_SEQUENCE],'mp4')
+    videoFiles = sorted([VIDEO_DIR+name for name in os.listdir(VIDEO_DIR)])[:10]
+    vid = imageio.get_reader(videoFiles[CUR_SEQUENCE],'mp4')
     NUMBER_OF_FRAMES = vid._meta['nframes']
     print("Switched to sequence: {}".format(CUR_SEQUENCE))
     print("# of frames: {}".format(NUMBER_OF_FRAMES))
+    
+    _3DCoordFiles = sorted([BALL_3D_COORDS_DIR+name for name in os.listdir(BALL_3D_COORDS_DIR)])
+    try:
+        CUR_3D_FILE = open(_3DCoordFiles[CUR_SEQUENCE])
+        READER_3D = csv.DictReader(CUR_3D_FILE)
+        print("Reading 3D coordinates from {}".format(_3DCoordFiles[CUR_SEQUENCE]))
+    except:
+        print("Couldn't find a file of 3D coordinates for this sequence")
+    
+    
     
 def updateFrame(frameNum):
     global CUR_FRAME    
@@ -98,31 +122,15 @@ def updateFrame(frameNum):
 #                            Globals End                                    #
 #############################################################################
 
+#############################################################################
+#                           Utility Functions Begin                         #
+#############################################################################
+def getPointOneAway(p1, p2):
+    return (p2-p1) / (np.linalg.norm(p2-p1)) + p1
+#############################################################################
+#                           Utility Functions End                           #
+#############################################################################
 
-#############################################################################
-#                           Utility function begin
-#############################################################################
-def getRotation(a, b):
-    a = a.reshape((1,3))
-    b = b.reshape((1,3))
-        
-    print("A: {}".format(a))
-    print("B: {}".format(b))
-    
-    v = np.cross(a,b)       # a X b
-    s = np.linalg.norm(v)   # magnitude of v
-    c = np.dot(a,b)
-    
-    skew = np.array([[    0, -v[2],  v[1]],
-                     [ v[2],     0, -v[0]],
-                     [-v[1],  v[0],    0]])
-                     
-    R = np.identity(3) + skew + np.matmul(np.square(skew),1/(1+c))
-    return R
-
-#############################################################################
-#                           Utility function end
-#############################################################################
 class Camera:
     def __init__(self):
         self.speed = 0.1
@@ -143,10 +151,10 @@ class Camera:
         self.pos -= self.speed * self.front
         
     def moveLeft(self):
-        self.pos -= np.linalg.norm(np.cross(self.front, self.up)) * self.speed
+        self.pos -= np.cross(self.front, self.up) * self.speed
         
     def moveRight(self):
-        self.pos += np.linalg.norm(np.cross(self.front, self.up)) * self.speed
+        self.pos += np.cross(self.front, self.up) * self.speed
         
     def lookUp(self):
         self.pitch += 1
@@ -168,13 +176,12 @@ class Camera:
         self.front[0] = np.cos(np.deg2rad(self.pitch)) * np.cos(np.deg2rad(self.yaw))
         self.front[1] = np.sin(np.deg2rad(self.pitch))
         self.front[0] = np.cos(np.deg2rad(self.pitch)) * np.sin(np.deg2rad(self.yaw))
-        
-        
 
 class GL3DPlot(QGLWidget):
     def __init__(self, parent):
         QGLWidget.__init__(self, parent)
-        self.setFocusPolicy(Qt.TabFocus)
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.grabKeyboard()
         self.cam = Camera()
         
     def paintGL(self):
@@ -184,7 +191,58 @@ class GL3DPlot(QGLWidget):
         gluPerspective(self.cam.fovy, self.cam.aspectRatio, self.cam.zNear, self.cam.zFar)
         self.lookAt()
         
+        self.drawAxes()
+        self.drawCameras()
+        self.drawTable()
+        self.drawBalls()
+        
+        glEnableClientState(GL_VERTEX_ARRAY)
+        
+        glFlush()
+        
+    def drawBalls(self):
+        global READER_3D
+        global CUR_3D_FILE
+        if READER_3D != None:
+            CUR_3D_FILE.seek(0)
+            for row in READER_3D:
+                try:
+                    frame = int(row['Frame'])
+                    x = float(row['x'])
+                    y = float(row['y'])
+                    z = float(row['z'])
+                except:
+                    continue
+                    
+                if frame > CUR_FRAME:
+                    break
+                    
+                if frame == CUR_FRAME:
+                    glColor3f(1, 0, 0)
+                else:
+                    glColor3f(1,.5, 0)
+                
+                glPushMatrix();
+                glTranslatef(x, y, z)
+                glutSolidSphere(.02, 20, 10)
+                glPopMatrix();
+       
+    def drawTable(self):
+        length = 2.74
+        width = 1.525
+        height = .76
+        glPushMatrix()
+        glBegin(GL_POLYGON)
+        glVertex3f(-length/2, -width/2, height)
+        glVertex3f( length/2, -width/2, height)
+        glVertex3f( length/2,  width/2, height)
+        glVertex3f(-length/2,  width/2, height)
+        glEnd()
+        glPopMatrix()
+        
+    def drawAxes(self):
         #draw x, y, and z lines
+        glPushMatrix()
         glColor4f(0,0,0,1)
         glBegin(GL_LINES)
         glVertex3f(0, 0,  100)
@@ -194,32 +252,45 @@ class GL3DPlot(QGLWidget):
         glVertex3f( 100, 0, 0)
         glVertex3f(-100, 0, 0)
         glEnd()
+        glPopMatrix()
         
-        
+    def drawCameras(self):
+        glPushMatrix()
         #draw camera i, j, and k vectors
         glBegin(GL_LINES)
-        glColor4f(1, 0, 0,.3)
-        for i in (c1, R1[0,:], c2, R2[0,:], c3, R3[0,:]):
-            glVertex3f(i[0], i[1], i[2])
+        
+        glColor3f(1, 0, 0)
+        for p1, p2 in ((c1, R1[0,:]), (c2, R2[0,:]), (c3, R3[0,:])):
+            p1 = p1.reshape((1,3))[0]
+            glVertex3f(p1[0], p1[1], p1[2])
+            drawP = getPointOneAway(p1, p2)
+            glVertex3f(drawP[0], drawP[1], drawP[2])
+        
+        glColor3f(0, 1, 0)
+        for p1, p2 in ((c1, R1[1,:]), (c2, R2[1,:]), (c3, R3[1,:])):
+            p1 = p1.reshape((1,3))[0]
+            glVertex3f(p1[0], p1[1], p1[2])
+            drawP = getPointOneAway(p1, p2)
+            glVertex3f(drawP[0], drawP[1], drawP[2])
             
-        glColor4f(0, 1, 0, .3)
-        for i in (c1, R1[1,:], c2, R2[1,:], c3, R3[1,:]):
-            glVertex3f(i[0], i[1], i[2])
-            
-        glColor4f(0, 0, 1, .3)
-        for i in (c1, R1[2,:], c2, R2[2,:], c3, R3[2,:]):
-            glVertex3f(i[0], i[1], i[2])
+        glColor3f(0, 0, 1)
+        for p1, p2 in ((c1, R1[2,:]), (c2, R2[2,:]), (c3, R3[2,:])):
+            p1 = p1.reshape((1,3))[0]
+            glVertex3f(p1[0], p1[1], p1[2])
+            drawP = getPointOneAway(p1, p2)
+            glVertex3f(drawP[0], drawP[1], drawP[2])
         glEnd()
+        glPopMatrix()
         
-        glEnableClientState(GL_VERTEX_ARRAY)
-        
-        glFlush()
+    def draw3DArrow(self, length=5):
+        gluCylinder(gluNewQuadric(), 1, 1, length, 10, 10)
+        glTranslatef(0, 0, length)
+        gluCylinder(gluNewQuadric(), 3, 0, length/5, 100, 100)
 
     def resizeGL(self, w, h):
         '''
         Resize the GL window 
         '''
-        print('Resizing GL')
         glViewport(0, 0, w, h)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
@@ -229,9 +300,11 @@ class GL3DPlot(QGLWidget):
         '''
         Initialize GL
         '''
-        print("Initializing GL")
+        glutInit()
         glClearColor(1.0, 1.0, 1.0, 1.0)
         glClearDepth(1.0)
+        glutMouseFunc(self.mouse)
+        glutMotionFunc(self.motion)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         
@@ -245,18 +318,7 @@ class GL3DPlot(QGLWidget):
                   center[0], center[1], center[2],
                   c.up[0],  c.up[1],  c.up[2])
         
-    def keyPressEvent(self, event):
-        global SHIFT
-        global CTRL
-        
-        if event.key() == 16777248:
-            SHIFT = not SHIFT
-        if event.key() == 16777249:
-            CTRL = not CTRL
-        
-        if event.key() == 70:
-            self.fovy += 1 
-        
+    def keyPressEvent(self, event):        
         if event.key() == 87:
             self.cam.moveForward()
         if event.key() == 83:
@@ -277,6 +339,40 @@ class GL3DPlot(QGLWidget):
         
         self.updateGL()
         
+        
+    def mouse(self, button, state, x, y):
+        global MOUSE_BUTTON
+        global MOVING
+        global START_X
+        global START_Y
+        print("Mouse registered")
+        if state == GLUT_DOWN:
+            MOUSE_BUTTON = button
+            MOVING = 1
+            START_X = x
+            START_Y = y
+        
+        if state == GLUT_UP:
+            MOUSE_BUTTON = button
+            MOVING = 0
+
+    def motion(self, x, y):
+        global MOVING
+        global MOUSE_BUTTON
+        global ANGLE
+        global ANGLE2
+        global START_X
+        global START_Y
+        print("Motion registered")
+        if MOVING:
+            if MOUSE_BUTTON == GLUT_LEFT_BUTTON:
+                ANGLE += (x - START_X)
+                angle2 += (y - START_Y)
+            START_X = x
+            START_Y = y
+            self.updateGL()
+            
+        
 class VideoFrameDisplay(QGroupBox):
     def __init__(self, cName, parent):
         QGroupBox.__init__(self, parent)
@@ -291,10 +387,18 @@ class VideoFrameDisplay(QGroupBox):
         self.imgBox = QLabel()
         
     def createLayout(self):
+        
         self.mainBox = QVBoxLayout()
         self.mainBox.addWidget(self.pathLabel)
         self.mainBox.addWidget(self.imgBox)
         self.setLayout(self.mainBox)
+        '''
+        self.padding = QHBoxLayout()
+        self.padding.addStretch(1)
+        self.padding.addLayout(self.mainBox)
+        self.padding.addStretch(1)
+        self.setLayout(self.padding)
+        '''
         
     def updateSequence(self):
         self.vid = imageio.get_reader(VIDEO_DIR + self.videoFiles[CUR_SEQUENCE],'mp4')
@@ -396,6 +500,8 @@ class PPDashBoard(QWidget):
         self.vidFrame1.updateImg()
         self.vidFrame2.updateImg()
         self.vidFrame3.updateImg()
+        self.gl3dPlot.updateGL()
+        
             
 
 class PPApplication(QMainWindow):
